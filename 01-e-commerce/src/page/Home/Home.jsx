1ac795a19d9ee01f73dacd395d80025aa981f7b6
David Alvarez <david.alvgar23@gmail.com>
2023-06-20 19:26:45 -0700

    Home 90% (Falta de imagenes)

--- 01-e-commerce/src/page/Home/Home.jsx
+++ 01-e-commerce/src/page/Home/Home.jsx
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react'
+import { getAllItems } from '@/services/itemService'
+import { NavLink } from 'react-router-dom'
+import './Home.css'
+
 const Home = () => {
+  const [itemsData, setItemsData] = useState(null)
+
+  useEffect(() => {
+    const fetchItemData = async () => {
+      try {
+        const response = await getAllItems()
+        if (response.status === 200) {
+          setItemsData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error:', error.message)
+      }
+    }
+    fetchItemData()
+  }, [])
   return (
-    <div>Home</div>
+    <div>
+      <p className='header'>Products</p>
+      <div className='container'>
+        {itemsData && itemsData.map((product) => (
+          <div className='card' style={{ width: '18rem' }} key={product.id}>
+            <img className='card-img-top' style={{ maxHeight: '200px' }} src={product.image} alt={product.product_name} />
+            <div className='card-body'>
+              <div className='body'>
+                <h5 className='card-title'>{product.product_name}</h5>
+                <NavLink to='/shopping/card' className='plus'>+</NavLink>
+              </div>
+              <p className='price'>${product.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 export default Home
--- 01-e-commerce/src/routes/index.jsx
+++ 01-e-commerce/src/routes/index.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../page/Home'
 import Login from '../page/Login'
 import SignUp from '../page/SignUp'
+import Card from '../page/ShoppingCard'
+import Detail from '../page/Detail/Detail'
 
 const RoutesIndex = () => {
   return (
@@ -9,6 +11,8 @@ const RoutesIndex = () => {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
+      <Route path='/shopping/card' element={<Card />} />
+      <Route path='/detail' element={<Detail />} />
     </Routes>
   )
 }
