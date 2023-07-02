57c3c75a6e535049145d987d533fe1ccabc069d7
David Alvarez <david.alvgar23@gmail.com>
2023-07-01 20:07:48 -0700

    modified:   01-e-commerce/src/routes/index.jsx

--- 01-e-commerce/src/routes/index.jsx
+++ 01-e-commerce/src/routes/index.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
-/* import { useAuthContext } from '@/hooks/useAuth' */
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from '@/hooks/useAuth'
 import Home from '../page/Home'
 import Login from '../page/Login'
 import SignUp from '../page/SignUp'
@@ -8,7 +8,7 @@ import Detail from '../page/Detail/Detail'
 import Add from '../page/Add'
 
 const RoutesIndex = () => {
-  /* const { isAuth } = useAuthContext() */
+  const { isAuth, isAdmin } = useAuthContext()
 
   return (
     <Routes>
@@ -16,11 +16,19 @@ const RoutesIndex = () => {
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
       <Route
-        path='/shopping/card' element={<Card />}
+        path='/shopping/card' element={isAuth
+          ? (<><Card /></>)
+          : (<><Navigate to='/login' /></>)}
       />
       <Route path='/detail/:id' element={<Detail />} />
       <Route
-        path='/add' element={<Add />}
+        path='/add' element={isAuth
+          ? (
+            <>{isAdmin
+              ? (<><Add /></>)
+              : (<><Navigate to='/' /></>)}
+            </>)
+          : (<><Navigate to='/login' /></>)}
       />
 
     </Routes>
